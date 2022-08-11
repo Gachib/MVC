b48bc33b88c5eaa10bd1e4e86c4f2b33de036c41
Gachib <brayan.aruquipa00@gmail.com>
2022-08-11 19:22:39 -0300

    fixed static

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 const mainRouter = require('./routes/mainRouter');
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use('/', mainRouter); 
 
